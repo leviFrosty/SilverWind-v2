9d59bfe12ed177869b8c5b9c3e676bf88d4a70b7
Levi Wilkerson <chelleas123@gmail.com>
2021-12-14 23:41:40 -0500

    init

---
+++ components/layout.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Layout({ children, home }) {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Luxury handmade jewerly crafted by Julia."
+        />
+      </Head>
+      <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+      <footer>Footer</footer>
+    </div>
+  );
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Layout from "/components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First post!</title>
+      </Head>
+      <h1>First post!</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+}
