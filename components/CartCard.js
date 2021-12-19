420c0ae94f84cda764044b8819048fb5e4f0f751
Levi Wilkerson <chelleas123@gmail.com>
2021-12-19 15:48:54 -0500

    added cart data fetching

---
+++ components/CartCard.js
@@ -0,0 +1,8 @@
+
+export default function CartCard({product, quantity}) {
+  return (
+    <article>
+      <h2>{product.name}</h2>
+    </article>
+  )
+}
--- pages/cart.js
+++ pages/cart.js
@@ -1,23 +1,35 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import CenterTitle from "../components/CenterTitle";
 import Layout from "../components/layout";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import UserContext from "../contexts/userContext";
-
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../lib/fbInstance";
+import CartCard from "../components/CartCard";
 
 export default function Cart() {
   const user = useContext(UserContext);
   const router = useRouter();
+  const [cartProducts, setcartProducts] = useState([]);
+  const [userCart, setUserCart] = useState([]);
   
-  useEffect(() => {
+  useEffect(async () => {
     if (!user) return router.replace("/login");
-  }, [])
-
-
+    const ref = await doc(db, "users", user.uid);
+    const { cart } = await (await getDoc(ref)).data();
+    let arr = [];
+    cart.forEach(async (item) =>
+      arr.push(await (await getDoc(doc(db, "products", item.id))).data())
+    );
+    setcartProducts(arr);
+  }, []);
 
   return (
     <Layout>
       <CenterTitle>Cart</CenterTitle>
+      {cartProducts.map((product) => (
+        <CartCard key={product.id} product={product} />
+      ))}
     </Layout>
-  )
+  );
 }
