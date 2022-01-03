7e440c099c31b73c503e81b1b37790756242317d
Levi Wilkerson <chelleas123@gmail.com>
2022-01-03 00:15:04 -0500

    added cart number

--- components/nav.js
+++ components/nav.js
@@ -5,9 +5,12 @@ import Bars from "../public/icons/bars-solid.svg";
 import ShoppingBasket from "../public/icons/shopping-basket-solid.svg";
 import ProfileIcon from "../public/icons/profile-icon.svg";
 import NavLink from "./NavLink";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../lib/fbInstance";
 
 export default function Nav() {
   const [isCollapsed, setCollapsed] = useState(true);
+  const [cartSize, setCartSize] = useState(0);
   const { user } = useContext(UserContext);
 
   const onWindowResize = () => {
@@ -18,8 +21,23 @@ export default function Nav() {
   };
 
   useEffect(() => {
+    let unsubscribeCart = () => {};
+    if (user) {
+      unsubscribeCart = onSnapshot(doc(db, "users", user.uid), (doc) => {
+        const cart = doc.data().cart;
+        let total = 0;
+        cart.forEach((item) => (total = total + item.quantity));
+        console.log(total);
+        setCartSize(total);
+      });
+    }
+    return () => unsubscribeCart();
+  }, [user]);
+
+  useEffect(() => {
     if (window.innerWidth > 768) setCollapsed(false);
     window.addEventListener("resize", onWindowResize);
+
     return () => window.removeEventListener("resize", onWindowResize);
   }, []);
 
@@ -91,8 +109,13 @@ export default function Nav() {
               </a>
             </Link>
             <Link href="/cart">
-              <a className="bg-violet-500 shadow-lg rounded-md transition-all px-6 py-3 w-full md:py-1 text-white gap-2 justify-center hover:bg-violet-700 ">
+              <a className="bg-violet-500 relative flex shadow-lg rounded-md transition-all px-6 py-3 w-full md:py-1 text-white gap-2 justify-center hover:bg-violet-700 ">
                 <ShoppingBasket className="w-5 mx-auto" alt="Cart" />
+                {cartSize === 0 ? null : (
+                  <span className="absolute m-0 -right-1 md:-bottom-3 md:-right-1 text-violet-500 bg-white border-violet-500 border-2 text-md md:text-xs rounded-full px-4 py-1 md:px-2 md:py-1">
+                    {cartSize}
+                  </span>
+                )}
               </a>
             </Link>
           </div>
