60a06c0b499f9d2c94fc6f690cf3c3a30cf53226
Levi Wilkerson <chelleas123@gmail.com>
2022-01-11 03:55:11 -0500

    feat: add index twit meta tags and image meta tags

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,30 @@ export default function Home() {
           name="description"
           content="SilverWind is offering the best in custom handcrafted Jewelry and bespoke pieces. Our goal is to deliver a quality and unique experience that will leave you feeling special with something special."
         />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://www.silverwind.store/products/rings"
+        />
+        <meta property="og:title" content="SilverWind - Rings" />
+        <meta
+          property="og:description"
+          content="SilverWind is offering the best in custom handcrafted Jewelry and bespoke pieces. Our goal is to deliver a quality and unique experience that will leave you feeling special with something special."
+        />
+        <meta property="og:image" content="" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://www.silverwind.store/products/rings"
+        />
+        <meta property="twitter:title" content="SilverWind - Rings" />
+        <meta
+          property="twitter:description"
+          content="SilverWind is offering the best in custom handcrafted Jewelry and bespoke pieces. Our goal is to deliver a quality and unique experience that will leave you feeling special with something special."
+        />
+        <meta property="twitter:image" content="" />
       </Head>
       <section>
         <Hero />
