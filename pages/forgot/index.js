de38b08df5cd4bfb7556e6aa3761484630312078
Levi Wilkerson <chelleas123@gmail.com>
2022-01-15 20:03:32 -0500

    style: reset password page

--- pages/forgot/index.js
+++ pages/forgot/index.js
@@ -7,6 +7,9 @@ import Input from "../../components/input";
 import { auth } from "../../lib/fbInstance";
 import Head from "next/head";
 import { siteTitlePrefix } from "../../components/layout";
+import CenterTitle from "../../components/CenterTitle";
+import Form from "../../components/form";
+import Container from "../../components/Container";
 
 export default function Forgot() {
   const [email, setemail] = useState("");
@@ -26,24 +29,37 @@ export default function Forgot() {
         <title>{siteTitlePrefix} Reset Your Password</title>
         <meta
           name="description"
-          content="Learn how to change or rest your SilverWind password."
+          content="Learn how to change or reset your SilverWind password."
         />
       </Head>
-      <h1>Reset your Password</h1>
-      <form onSubmit={handleFormSubmit}>
-        <Input
-          name="email"
-          title="email"
-          type="email"
-          value={email}
-          setState={setemail}
-          required
-        />
-        <button type="submit">Reset Password</button>
-      </form>
-      <Link href="/login">
-        <a>Back to login</a>
-      </Link>
+
+      <Container className="flex flex-col justify-center">
+        <CenterTitle>Reset your Password</CenterTitle>
+        <Form
+          onSubmit={handleFormSubmit}
+          className="min-w-12 max-w-screen-sm mx-auto flex flex-col gap-2"
+        >
+          <Input
+            name="email"
+            title="email"
+            type="email"
+            value={email}
+            setState={setemail}
+            required
+          />
+          <button
+            className="text-white rounded-md px-3 py-2 bg-violet-500 hover:bg-violet-700 active:bg-violet-700"
+            type="submit"
+          >
+            Reset Password
+          </button>
+        </Form>
+        <Link href="/login">
+          <a className="mx-auto text-violet-900 underline decoration-2 my-2 font-semibold">
+            Back to login
+          </a>
+        </Link>
+      </Container>
     </Layout>
   );
 }
