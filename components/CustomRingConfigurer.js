81517eebe4098fbdb4dc124241d03e882e2b6719
Levi Wilkerson <chelleas123@gmail.com>
2022-01-18 02:40:37 -0500

    feat: add CustomRingConfigurer component for Admin Dashboard

--- components/CustomRingConfigurer.js
+++ components/CustomRingConfigurer.js
@@ -1,95 +1,79 @@
-import Head from "next/head";
-import React from "react";
-import Layout, { siteTitlePrefix } from "./layout";
-
+import React, { useState } from "react";
+import CenterTitle from "./CenterTitle";
+import Input from "./input";
+import Stone from "../classes/Stone";
+import Container from "./Container";
+import NewStone from "./CustomOrderFormAdminSections/NewStone";
+import ChevronRight from "../public/icons/chevron-right-solid.svg";
 
 export default function CustomRingConfigurer() {
+  const [isStoneOpen, setisStoneOpen] = useState(true);
 
-
-const exampleOutput = {
-  // General Options
-  stones: [
-    {
-      id: "someid1231252341",
-      image: "https://someimgurl/",
-      description: "Stone name",
-      quantity: 10,
-      size: "1x3",
-    },
-  ],
-  stoneSettings: [
-    {
-      id: "someid1231252341",
-      image: "https://someimgurl/",
-      description: "Stone name",
-    },
-  ],
-  backExposed: Boolean,
-  // Category Specific
-  categories: [
-    {
-      name: "necklaces",
-      options: {
-        lengths: [8, 10, 12, 14, 16],
-      },
-    },
-    ,
-    {
-      name: "rings",
-      options: {
-        bands: [
-          {
-            id: "someid1231252341",
-            image: "https://someimgurl/",
-            description: "Stone name",
-          },
-        ],
-      },
-    },
-    {
-      name: "earrings",
-      options: {
-        earringType: [
-          { name: "stud", image: "https://someimgurl/", id: "someid" },
-          { name: "dangly", image: "https://someimgurl/", id: "someid" },
-        ],
-      },
-    },
-  ],
-};
-
-
-  // Form
-  // Stones
-  // Settings
-  // Necklaces
-  //  - Lengths
-  // Rings
-  //  - Bands
-  // Earrings
-  //  - Type
-
-  // Desired Output For End User Selection Process:
-  // Descriptions on each user input section
-  // Category ? Necklace / Ring / Earring / etc.
-  // Stone ? (Pull from list of stones available)
-  // Setting ? Bezel Wire Types
-  // Switch (category)
-  // case (necklace):
-  //  - length of chain options
-  //  - personalization (initials stamped - max length 2)
-  // case (Ring):
-  //  - band selections
-  //  - personalization (initials / word - max length 10)
-  //  case (earrings):
-  //  - Studs / dangly(hanging) earrings?
-  // Notes for order (specifications)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  };
 
   return (
-    <Layout>
-      <Head>
-        <title>{siteTitlePrefix} </title>
-      </Head>
-    </Layout>
+    <Container>
+      <h2 className="text-lg font-bold text-violet-900 inline mx-auto mt-1">
+        Custom Order Settings
+      </h2>
+      <div className="p-2">
+        <button
+          className="flex w-full items-center"
+          onClick={() => setisStoneOpen(!isStoneOpen)}
+        >
+          {" "}
+          <h2 className="text-lg font-bold text-violet-900 inline">
+            New Stone
+          </h2>
+          <span>
+            <ChevronRight
+              style={isStoneOpen ? { transform: "rotate(90deg)" } : {}}
+              className="ml-1 w-4 h-4 transition-transform text-violet-900"
+            />
+          </span>
+        </button>
+        {isStoneOpen ? (
+          <NewStone isStoneOpen={isStoneOpen} setisStoneOpen={setisStoneOpen} />
+        ) : null}
+        <h2 className="text-lg font-bold text-violet-900">New Setting</h2>
+        <Input />
+        <h2 className="text-lg font-bold text-violet-900">Necklace Length</h2>
+        <Input />
+        <h2 className="text-lg font-bold text-violet-900">New Ring Band</h2>
+        <Input />
+        <h2 className="text-lg font-bold text-violet-900">New Earring Type</h2>
+        <Input />
+      </div>
+    </Container>
   );
 }
+
+// Notes:
+// Form
+// Stones
+// Settings
+// Necklaces
+//  - Lengths
+// Rings
+//  - Bands
+// Earrings
+//  - Type
+
+// Desired Output For End User Selection Process:
+// Descriptions on each user input section
+// Category ? Necklace / Ring / Earring / etc.
+// Stone ? (Pull from list of stones available)
+// Setting ? Bezel Wire Types
+// Switch (category)
+// case (necklace):
+//  - length of chain options
+//  - personalization (initials stamped - max length 2)
+// case (Ring):
+//  - band selections
+//  - personalization (initials / word - max length 10)
+//  case (earrings):
+//  - Studs / dangly(hanging) earrings?
+// Notes for order (specifications)
