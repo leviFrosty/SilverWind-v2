1dc01b2088fea7141afc3e839d3561bec1dc695d
Levi Wilkerson <chelleas123@gmail.com>
2022-01-18 02:40:06 -0500

    style: add margin on error

--- components/form.js
+++ components/form.js
@@ -4,7 +4,7 @@ export default function Form({ onSubmit, error, children, ...props }) {
   return (
     <form onSubmit={onSubmit} {...props}>
       {children}
-      {error ? <p className="font-bold text-red-600 text-center">{error}</p> : null}
+      {error ? <p className="font-bold text-red-600 text-center mt-2">{error}</p> : null}
     </form>
   );
 }
