190b0f4f75552e9525d64b9f56d3175c06780875
Levi Wilkerson <chelleas123@gmail.com>
2022-01-20 18:39:13 -0500

    style: add invalid outline for inputs

--- components/InputStyled.js
+++ components/InputStyled.js
@@ -10,7 +10,7 @@ export default function InputStyled({ name, title, children, ...props }) {
         {title}
       </label>
       <input
-        className="file:rounded-md mb-4 file:mr-2 text-violet-900 file:bg-violet-500 file:text-white file:px-5 file:py-1 file:border-0 file:hover:bg-violet-600 placeholder:text-gray-500 placeholder:opacity-50 rounded-md focus:border-violet-600 block w-full bg-violet-100 border-violet-200 px-3 py-2 focus:ring-violet-500"
+        className="focus:invalid:border-red-300 focus:invalid:outline-red-300 file:rounded-md mb-4 file:mr-2 text-violet-900 file:bg-violet-500 file:text-white file:px-5 file:py-1 file:border-0 file:hover:bg-violet-600 placeholder:text-gray-500 placeholder:opacity-50 rounded-md focus:border-violet-600 block w-full bg-violet-100 border-violet-200 px-3 py-2 focus:ring-violet-500"
         name={name}
         {...props}
       >
