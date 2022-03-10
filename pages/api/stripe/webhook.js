8a8f8824d536ce6d288ea85fe37c78218661b102
Levi Wilkerson <chelleas123@gmail.com>
2022-03-09 21:01:50 -0500

    fix: removed fb admin shared module for ssr action

--- pages/api/stripe/webhook.js
+++ pages/api/stripe/webhook.js
@@ -1,7 +1,21 @@
 import Stripe from "stripe";
 import { buffer } from "micro";
-import admin from "../../../lib/fbAdminInstance";
+import * as admin from "firebase-admin";
 
+const { private_key } = JSON.parse(process.env.ADMIN_PRIVATE_KEY);
+
+// Init firebase admin sdk
+const ADMIN_FIREBASE_CREDS = {
+  projectId: process.env.ADMIN_PROJECT_ID,
+  privateKey: private_key,
+  clientEmail: process.env.ADMIN_CLIENT_EMAIL,
+};
+if (admin.apps.length === 0) {
+  admin.initializeApp({
+    credential: admin.credential.cert(ADMIN_FIREBASE_CREDS),
+    databaseURL: "https://silverwind-ca60d.firebaseio.com",
+  });
+}
 const db = admin.firestore();
 
 // Init stripe admin sdk
